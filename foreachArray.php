b2ce7b04d4795162ddd4dd08f3a900202ca90564
leandro falcao <leandrofalcaoti@gmail.com>
2021-02-18 01:22:08 -0300

    Create foreachArray.php

---
+++ foreachArray.php
@@ -0,0 +1,11 @@
+?php 
+ 
+   $ingredientes = [ 'uma fatia de queijo', 'uma rodela de tomate',  'duas folhas de alface'];
+
+      foreach($ingredientes as $ingrediente ) {
+         print "$ingrediente";
+      }
+   
+?>
+
+// vai sair os ingredientes no print
