bdfe725356064f5941968c62bc05b6b4530c5302
leandro falcao <leandrofalcaoti@gmail.com>
2021-02-18 18:26:58 -0300

    Update paginaWebTabelaAlunosArrayForeach.php

--- paginaWebTabelaAlunosArrayForeach.php
+++ paginaWebTabelaAlunosArrayForeach.php
@@ -8,18 +8,45 @@
       <title>criando tabela com nome e nota dos alunos</title>
    </head>
 
+      <?php 
+         $Alunos_notas = ['Leandro' => 9.5, 'Bernado' => 8, 'Carmen' => 8.5, 'Joao' => 7, 'Maria' => 6.4];
+      ?>
+    
+
    <body>
       <h1>tabela aluno e nota usando o foreach</h1>
 
-      <?php
-         $Alunos_notas = ['Leandro' => 9.5, 'Bernado' => 8, 'Carmen' => 8.5, 'Joao' => 7, 'Maria' => 6.4];
+      <table>
+         
+         <thead>
+            <th>nome</th>
+            <th>nota</th>
+         </thead>
 
-         foreach ($Alunos_notas as $aluno => $nota) {
-            print "$aluno: $nota. \n <br>";
-         }
+         <tbody>
+            
+            <?php
+               $Alunos_notas = ['Leandro' => 9.5, 'Bernado' => 8, 'Carmen' => 8.5, 'Joao' => 7, 'Maria' => 6.4]; 
+               foreach ($Alunos_notas as $aluno => $nota):
+            ?>   
+         
+            
+            <?php /* vamos eslaçar o aluno e depois sua nota em cada td via php */ ?>
+            <tr>
 
-      ?>
+               <td><?php print $aluno ?></td>
+               <td><?php print $nota ?></td>
+            </tr>
+            <?php endforeach; ?>
+         </tbody>
+      </table>
+
 
    </body>
 
 </html>
+
+   <?php
+      // atualizar a cada 30 segundos
+       header('Refresh:30');
+   ?>
