634ebb6803220d2fbbe5cdf31be73adf71730df4
leandro falcao <leandrofalcaoti@gmail.com>
2021-02-20 11:51:38 -0300

    Update applicacaoCurriculo.php

--- applicacaoCurriculo.php
+++ applicacaoCurriculo.php
@@ -51,7 +51,7 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title> <?= $nome ?> </title>
+   <title> curriculo <?= $nome ?> </title>
 </head>
    <body>
       <h1> <?= $nome ?> </h1>      
