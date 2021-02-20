0d79cd6a8fcff64827f1f71b0e0c6adf81b922df
leandro falcao <leandrofalcaoti@gmail.com>
2021-02-19 23:50:41 -0300

    Update applicacaoCurriculo.php

--- applicacaoCurriculo.php
+++ applicacaoCurriculo.php
@@ -3,4 +3,5 @@
    $nome =  'leandro falcao';
    $profisao = 'programador ';
    $especialidade = 'programacao web front end';
+   $contatos = ['email' => 'leandrofalcao@gmail.com', 'gihub' => 'github.com/leandro-falcao']
 ?>
