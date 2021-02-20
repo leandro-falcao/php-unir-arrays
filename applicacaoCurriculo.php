e21db01ad519378ca5d1852f2cf8451fb5992ce5
leandro falcao <leandrofalcaoti@gmail.com>
2021-02-20 11:48:08 -0300

    Update applicacaoCurriculo.php

--- applicacaoCurriculo.php
+++ applicacaoCurriculo.php
@@ -3,6 +3,46 @@
    $nome =  'leandro falcao';
    $profisao = 'programador ';
    $especialidade = 'programacao web front end';
-   $contatos = ['email' => 'leandrofalcao@gmail.com', 'gihub' => 'github.com/leandro-falcao', 'linkedin' => 'linkedin.com/in/leandrofalcao'];
+   
+   
+   $contatos = ['email' => 'leandrofalcao@gmail.com', 
+     'gihub' => 'github.com/leandro-falcao', 
+      'linkedin' => 'linkedin.com/in/leandrofalcao'
+   ];
+
+   $formacao = [['inicio' => 2006, 
+      'termino' => 2012,
+      'instituicao' => 'unicsul',
+      'ocupacao' => 'tecnologo'], 
+      ['inicio' => 2012, 
+      'termino' => false,
+      'instituicao' => 'cruzeiro',
+      'ocupacao' => 'curso']];
+
+   $experiencia = [['inicio' => 2013, 
+      'termino' => FALSE,
+      'instituicao' => 'estagioX',
+      'ocupacao' => 'estagio'],
+      ['inicio' => 2014,
+      'termino' => 2015,
+      'instituicao' => 'udele-udemy',
+      'ocupacao' => 'programacao web']
+   ];
+
+   $portifolio = [
+      ['titulo' => 'rodada de desenvolvimento',
+      'foto' => 'foto-desenvolvimento.jpg',
+      'ano' => 2013 ],
+      ['titulo' => 'banco de dados e desenvolvimento',
+      'foto' => 'foto-banco-dados.jpg',
+      'ano' => 2016]
+   ];
+
+   $habilidade = ['html' => 100,
+      'css' => 100,
+      'php' => 60,
+      'javascript' => 50,
+      'python' => 20];
+
 
 ?>
